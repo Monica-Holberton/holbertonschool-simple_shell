ca1acf71f8c1e5992080698593ae51619b1ccd77
Boodenator <boode1000@gmail.com>
2025-05-22 17:04:45 +0300

    check for file in path

---
+++ which.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+
+int file_present( char *filename,  char *directory) {
+    if
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+
+    }
+
+
+
+
+    }
+
+    free();
+    return 0;
+}
