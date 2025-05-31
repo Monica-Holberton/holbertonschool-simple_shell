07dc65681d54c3eb3137679ce2319efc9da2207a
Boodenator <boode1000@gmail.com>
2025-05-31 18:01:55 +0300

    modified setenv()

--- _setenv.c
+++ _setenv.c
@@ -4,13 +4,21 @@
 
 int _setenv(char *value, int overwrite)
 {
-    if(value == NULL){
+   char *new_value;
+
+   if (name == NULL || value == NULL){
+    return (-1);
+   }
+
+   if(name != NULL && value != NULL){
+    new_value = getenv(name);
+    if (new_value == NULL){
         return -1;
+        }
+    //else if(setenv(name, value, overwrite) == 0){
+        //return 0;
     }
-
-        name = value;
-    if(overwrite == 1){
-        return 0;
     }
+   }
 
-}
+
